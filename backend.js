ce7b41760a0311fa96448108c64d22900d95fe5f
Jonathan Brown <jbrown@mix-blockchain.org>
2017-12-23 14:39:15 +0700

    Connect to itemStoreIpfsSha256 contract.

---
+++ backend.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8645"));
+web3.eth.defaultBlock = "pending";
+
+const itemStoreIpfsSha256Abi = require('./mix-item-store/item_store_ipfs_sha256.abi.json');
+const itemStoreIpfsSha256Factory = web3.eth.contract(itemStoreIpfsSha256Abi);
+const itemStoreIpfsSha256Address = "0xab4aa658083cd937c40a369cbd2c81ef3939999f";
+const itemStoreIpfsSha256 = itemStoreIpfsSha256Factory.at(itemStoreIpfsSha256Address);
+
+
