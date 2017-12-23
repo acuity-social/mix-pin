954dd421735d878bb6cb36073e05af91ba85c499
Jonathan Brown <jbrown@mix-blockchain.org>
2017-12-23 14:57:45 +0700

    Watch for IPFS hashes.

--- backend.js
+++ backend.js
@@ -8,5 +8,14 @@ const itemStoreIpfsSha256Abi = require('./mix-item-store/item_store_ipfs_sha256.
 const itemStoreIpfsSha256Factory = web3.eth.contract(itemStoreIpfsSha256Abi);
 const itemStoreIpfsSha256Address = "0xab4aa658083cd937c40a369cbd2c81ef3939999f";
 const itemStoreIpfsSha256 = itemStoreIpfsSha256Factory.at(itemStoreIpfsSha256Address);
+const eventPublishRevision = "0xa42468235cfdba0d7adbc48b79ee2a88f02cf52f20de70c669aaad7fd3e21585";
 
+const multihashes = require('multihashes');
+
+web3.eth.filter({fromBlock: 0, toBlock: 'pending', address: itemStoreIpfsSha256Address, topics: [eventPublishRevision]}).watch(function(error, result) {
+  if (error) { callback(error); return; }
+  const ipfsHash = result.data.substr(66, 64);
+  const base58IpfsHash = multihashes.toB58String(multihashes.encode(Buffer.from(ipfsHash, "hex"), 'sha2-256'));
+  console.log(base58IpfsHash);
+});
 
