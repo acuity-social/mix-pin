0bf6f3121a897b2fd8836c7534fee5b0fda81af2
Jonathan Brown <jbrown@mix-blockchain.org>
2019-04-24 17:56:14 +0700

    Fix Parity IPC path.

--- main.mjs
+++ main.mjs
@@ -66,7 +66,7 @@ async function pinIpfsHash(ipfsHash) {
 }
 
 async function start() {
-	let parityIpcPath = process.env['HOME'] + '/.config/Acuity/parity.ipc'
+	let parityIpcPath = process.env['HOME'] + '/.local/share/io.parity.ethereum/jsonrpc.ipc'
 	let web3 = new Web3(new Web3.providers.IpcProvider(parityIpcPath, net))
 
 	let blockNumber = await web3.eth.getBlockNumber()
